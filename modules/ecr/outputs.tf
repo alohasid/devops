0fee879c5da6f36ac7f6c40671f81b301dbb8163
OLEKSII SYDORENKO <alohasid>
2026-07-10 12:42:20 +0300

    hm 8-9

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -2,3 +2,7 @@ output "ecr_repository_url" {
   description = "The URL of the created ECR repository"
   value       = aws_ecr_repository.repo.repository_url
 }
+
+output "repository_url" {
+  value = aws_ecr_repository.repo.repository_url
+}
--- outputs.tf
+++ outputs.tf
@@ -1,47 +1,40 @@
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 state bucket from s3_backend module"
-  value       = module.s3_backend.s3_bucket_arn
-}
-
-output "dynamodb_table_name" {
-  description = "The name of the DynamoDB lock table from s3_backend module"
-  value       = module.s3_backend.dynamodb_table_name
-}
-
 output "vpc_id" {
-  description = "The ID of the VPC from vpc module"
-  value       = module.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  description = "The IDs of public subnets from vpc module"
-  value       = module.vpc.public_subnet_ids
+  value = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-  description = "The IDs of private subnets from vpc module"
-  value       = module.vpc.private_subnet_ids
+  value = module.vpc.private_subnet_ids
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
 }
 
 output "ecr_repository_url" {
-  description = "The URL of the ECR repository from ecr module"
-  value       = module.ecr.ecr_repository_url
-}
-
-output "eks_cluster_endpoint" {
-  description = "The endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+  value = module.ecr.repository_url
 }
 
 output "eks_cluster_name" {
-  description = "The name of the Kubernetes cluster"
-  value       = module.eks.cluster_name
+  value = module.eks.cluster_name
 }
 
-output "jenkins_url" {
-  value = "Check Kubernetes Services for Jenkins LoadBalancer External IP"
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
 
-output "argocd_url" {
-  value = "Check Kubernetes Services for ArgoCD Server LoadBalancer External IP"
+output "jenkins_admin_password_command" {
+  value = module.jenkins.get_admin_password_command
+}
+
+output "jenkins_url_command" {
+  value = module.jenkins.get_url_command
+}
+
+output "database_endpoint" {
+  value = module.rds.endpoint
 }
